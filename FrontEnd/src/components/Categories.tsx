1cefcb35f29b3daa61f5dcb17858c2ca6800c1d7
NiAlCa <casiean2it@gmail.com>
2025-02-24 17:34:28 +0100

    first commit

---
+++ FrontEnd/src/components/Categories.tsx
@@ -0,0 +1,27 @@
+import { getCategories } from "../../lib/get-categories";
+
+export const Categories = async () => {
+
+    const categories = await getCategories();
+
+    if (categories.length === 0) return null;
+
+
+
+
+  return(
+      <section className="categories">
+        <h1 className="categories-title">Categories</h1>
+        <ul className="categories-list">
+          {categories.map((category) => (
+            <li key={category.slug} className="category-item">
+              <h2 className="category-name">{category.name}</h2>
+              <p className="category-description">{category.description}</p>
+              <img src={category.image} alt={category.name} className="category-image" />
+            </li>
+          ))}
+        </ul>
+      </section>
+
+)
+};
